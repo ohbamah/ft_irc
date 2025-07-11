ca43a7a20b3fdf42923cd8873e0c3e04244c8a8b
prandoclara <prando.c@live.fr>
2025-07-11 17:03:33 +0200

    clean

--- .objs/src/Req.d
+++
@@ -1,4 +0,0 @@
-.objs/./src/Req.obj: src/Req.cpp includes/Req.hpp includes/Server.hpp \
-  includes/Client.hpp includes/Socket.hpp inline/Socket.inl \
-  includes/Channel.hpp includes/Select.hpp inline/Server.inl \
-  includes/Irc.hpp includes/FControl.hpp includes/Utils.hpp
